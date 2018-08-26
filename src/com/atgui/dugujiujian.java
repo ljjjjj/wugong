49de4de08b1391dfc72310e64a8ea434eb4d02eb
ljjjjj <674480235@qq.com>
2018-08-26 21:05:26 +0800

    第二招

--- src/com/atgui/dugujiujian.java
+++ src/com/atgui/dugujiujian.java
@@ -3,6 +3,7 @@ package com.atgui;
 public class dugujiujian {
 	public static void main(String[] args) {
 		System.out.println("sssssssssssssssssssssss");
+		System.out.println("sssxxxxxxxxxxxxxxxxxxxxxxss");
 	}
 
 }
